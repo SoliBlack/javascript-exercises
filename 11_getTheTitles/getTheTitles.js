d055dcd4dbfb5257c94ab43386dd734aef93d21c
SoliBlack <SolitaireBlack21@gmail.com>
2023-08-02 19:56:59 -0400

    Completed 11_getTheTitles
    This one was a bit difficult for me because I didn't fully understand
    the map function. I kinda understand but I barely know how to use it. I
    need to get more practice with this.
    
    Oh and btw I'm not going to do exercise 12. I completely don't
    understand reduce(), new Date(), or exactly how to calculate the birth
    and death and put it into a lopp and identify which one is the oldest.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,20 @@
 const getTheTitles = function() {
+    let titles = [];
+    const books = [
+        {
+            title: 'Book',
+            author: 'Name'
+        },
+        {
+            title: 'Book2',
+            author: 'Name2'
+        }
+    ]
+    
+    for(let i = 0; i < books.length; i++){
+        titles.push(books[i].title);
+    }
+    return titles;
 
 };
 
